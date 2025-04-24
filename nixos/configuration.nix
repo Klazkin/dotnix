a7c2b2c93ab9eefa2e8ed2410883a158d4fee812
matpac <matvei.matpac@gmail.com>
2025-04-24 10:08:16 +0300

    add latest dot files

---
+++ nixos/configuration.nix
@@ -0,0 +1,263 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ config, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+
+  stylix = {
+  	enable = true;
+  	autoEnable = true;
+
+  	polarity = "dark";
+  	base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+  	# base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+  	image = ./wallpaper.jpg;
+
+  	cursor = {
+  		name = "Bibata-Modern-Ice";
+  		package = pkgs.bibata-cursors;
+  	};
+
+  	fonts = {
+  		serif = {
+	      package = pkgs.noto-fonts;
+	      name = "Noto Serif";
+	    };
+
+	    sansSerif = {
+	      package = pkgs.noto-fonts;
+	      name = "Noto Sans";
+	    };
+
+	    monospace = {
+	      package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+	      name = "JetBrainsMono Nerd Font";
+	    };
+
+	    emoji = config.stylix.fonts.monospace;
+  	};
+  };
+
+  nix.settings = {
+      experimental-features = "nix-command flakes";
+
+      # enables caching for hyprland, no longer needed
+      # substituters = ["https://hyprland.cachix.org"];
+      # trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  # Bootloader
+  boot = {
+      plymouth = {
+        enable = false;
+        theme = "bgrt"; # handeled by stylix
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ "loader_2" ];
+          })
+        ];
+      };
+
+      # Enable "Silent Boot"
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      loader.timeout = 3;
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Tallinn";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "et_EE.UTF-8";
+    LC_IDENTIFICATION = "et_EE.UTF-8";
+    LC_MEASUREMENT = "et_EE.UTF-8";
+    LC_MONETARY = "et_EE.UTF-8";
+    LC_NAME = "et_EE.UTF-8";
+    LC_NUMERIC = "et_EE.UTF-8";
+    LC_PAPER = "et_EE.UTF-8";
+    LC_TELEPHONE = "et_EE.UTF-8";
+    LC_TIME = "et_EE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+    enable = true;
+  };
+
+  # Gnome
+  services.xserver = {
+    displayManager.gdm.enable = true;
+   	desktopManager.gnome = {
+   	  enable = true;
+   	  extraGSettingsOverridePackages = [ pkgs.mutter ];
+   	  extraGSettingsOverrides = ''
+   	    [org.gnome.desktop.peripherals.touchpad]
+   	    click-method='default'
+   	    [org.gnome.mutter]
+   	    experimental-features=['scale-monitor-framebuffer']
+   	  '';
+    };
+  };
+
+  environment.gnome.excludePackages = (with pkgs; [
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    xterm # xterm..
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
+  ]);
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.matpac = {
+    isNormalUser = true;
+    description = "Matthew";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Allow auto-upgrade
+  # system.autoUpgrade.enable = true;
+  # system.autoUpgrade.allowReboot = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+      micro
+      wget
+      python313
+  	  ranger
+  	  bottom
+  	  zellij
+  	  # noto-fonts
+  	  # (pkgs.nerdfonts.override { fonts = [ "Noto" "JetBrainsMono" ]; })
+  	  # greetd.tuigreet
+  	  # npins
+  ];
+
+  programs.htop.enable = true;
+  programs.zsh.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # services.greetd = {
+      # enable = true;
+      # settings = {
+        # default_session = {
+          # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+          # user = "matpac";
+        # };
+      # };
+  # };
+
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.hyprland.enable = true;
+  #
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # hardware accel
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # amd graphics, use default mesa driver instead!!
+#   hardware.graphics.extraPackages = with pkgs; [
+#     amdvlk
+#   ];
+#
+#   hardware.graphics.extraPackages32 = with pkgs; [
+#     driversi686Linux.amdvlk
+#   ];
+
+  # manjaro mount
+
+  fileSystems."/mnt/manjaro" =
+  {
+  	device = "/dev/nvme0n1p2";
+  	# options = [ "uid=1000" "gid=100" "dmask=007" "fmask=117" "nofail" ];
+  	options = ["nofail"];
+  };
+
+}
---
+++ nixos/home.nix
@@ -0,0 +1,549 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "matpac";
+  home.homeDirectory = "/home/matpac";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  fonts.fontconfig.enable = true;
+
+  # Add Firefox GNOME theme directory
+  # home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
+
+  programs.firefox = {
+        enable = true;
+  #       profiles.default = {
+  #          name = "Default";
+  #          settings = {
+  #             "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+  #
+  #             # For Firefox GNOME theme:
+  #             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  #             "browser.tabs.drawInTitlebar" = true;
+  #             "svg.context-properties.content.enabled" = true;
+  #          };
+  #          userChrome = ''
+  #             @import "firefox-gnome-theme/userChrome.css";
+  #             @import "firefox-gnome-theme/theme/colors/dark.css";
+  #          '';
+  #       };
+  };
+
+  programs.direnv = {
+  	enable = true;
+  	nix-direnv.enable = true;
+  	enableZshIntegration = true;
+  };
+
+  nixpkgs = {
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = (_: true);
+      };
+  };
+
+  programs.git = {
+    enable = true;
+    userName  = "matpac";
+    userEmail = "matvei.matpac@gmail.com";
+  };
+
+  home.packages = with pkgs; [
+  		# noto-fonts enabled through stylix
+  		montserrat # font
+  		fastfetch
+  		lazygit # tui git client
+  		modrinth-app # minecraft client
+  		# zulu23 - java
+    	# (pkgs.nerdfonts.override { fonts = [ "Noto" "JetBrainsMono" ]; })
+    	gnome-tweaks
+    	dconf-editor
+    	gnome-settings-daemon
+    #	everforest-gtk-theme
+    	# gruvbox-gtk-theme
+    	adwsteamgtk
+    	# tokyonight-gtk-theme
+    	jetbrains.webstorm
+    	jetbrains.rust-rover
+    	discord
+    	pinta # paint
+        # steam, enabled in configuration.nix
+    	spotify-player # tui spotify client
+		# Development deps
+    	godot_4
+		# rustc
+    	# rustup
+    	# cargo-outdated
+    	# pkg-config
+    	# gcc
+    	# openssl
+    	# openssl.dev
+    	# clang
+    	# llvmPackages.bintools
+    	# lutris
+    	# wine
+    	# wine64
+    	# winetricks
+     bottles # sandboxed wine environments
+     nixd # nix language server
+     nil # also a nix lang server
+     via # keybaord configurator (for supported models)
+     # gruvbox-dark-icons-gtk
+     gruvbox-plus-icons
+     spot
+    	] ++ (with pkgs.gnomeExtensions; [
+    	  user-themes
+    	  appindicator
+    	  vitals
+    	  blur-my-shell
+    	  open-bar
+    	  just-perfection
+    	  burn-my-windows
+    	  dash-to-dock
+    	  date-menu-formatter
+    	  desktop-cube
+    	  accent-directories
+    	  dash-to-panel
+    	  useless-gaps
+    	  weather-or-not
+    	  unite
+    	  arc-menu
+    	  media-controls
+        quick-settings-audio-panel
+  ]);
+
+  programs.vscode = {
+       enable = true;
+       extensions = with pkgs.vscode-extensions; [
+         teabyii.ayu
+         eamodio.gitlens
+         usernamehw.errorlens
+         rust-lang.rust-analyzer
+         mkhl.direnv
+         esbenp.prettier-vscode
+         ms-azuretools.vscode-docker
+         sainnhe.gruvbox-material
+         bbenoist.nix
+         aaron-bond.better-comments
+         tamasfe.even-better-toml
+       ];
+
+      userSettings = {
+        # This property will be used to generate settings.json:
+        # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
+        "editor.formatOnSave" = true;
+        # "workbench.colorTheme" = lib.mkForce "Gruvbox Material Dark"; # overwrites stylix theme
+        "workbench.colorTheme" = lib.mkForce "Ayu Mirage Bordered";
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "workbench.iconTheme" = "ayu";
+        "window.titleBarStyle" = "custom";
+		"editor.fontLigatures" = true;
+      };
+  };
+
+  programs.bat = {
+    enable = true;
+  };
+
+  programs.zed-editor = {
+          enable = true;
+          extensions = ["nix" "toml" "elixir" "make"];
+
+          userSettings = {
+
+          	  lsp = {
+          	    rust-analyzer = {
+          	      binary = {
+          	        path_lookup = true;
+          	      };
+          	    };
+          	  };
+
+          	  assistant = {
+          	    enabled = true;
+          	    version = "2";
+          	    default_model = {
+          	      provider = "zed.dev";
+          	      model = "claude-3-5-sonnet-latest";
+          	    };
+          	  };
+
+          	  load_direnv = "shell_hook";
+          	  buffer_font_family = "JetBrainsMono Nerd Font";
+          	  ui_font_family = "Noto Sans";
+          	  buffer_font_weight = 200;
+
+          	  terminal = {
+          	    line_height = "standard";
+          	    env = {
+          	      TERM = "ghostty";
+          	    };
+          	  };
+
+          	  telemetry = {
+          	    diagnostics = true;
+          	    metrics = false;
+          	  };
+
+          	  ui_font_size = 24;
+          	  buffer_font_size = 22;
+
+          	  theme = {
+          	    mode = "system";
+          	    light = "One Light";
+          	    dark = "Gruvbox Dark Hard";
+          	  };
+
+          	  base_keymap = "JetBrains";
+
+          };
+  };
+
+  # symbolic links for custom gtk css
+  # xdg.configFile = {
+  #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+  #   "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+  #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  # };
+  #
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.thefuck.enable = true; # does it work?
+  programs.ghostty = {
+  	enable = true;
+  	settings = {
+  	      font-size = 14;
+  	      # font-family = "JetBrainsMonoNL Nerd Font";
+  	      background-opacity = 0.70;
+  	      background-blur-radius = 20;
+  	      theme = "GruvboxDarkHard";
+  	      # theme = "Ayu Mirage";
+  	      # window-decoration = none;
+
+  	      # The default is a bit intense for my liking
+  	      # but it looks good with some themes
+  	      unfocused-split-opacity = 1.0;
+
+  	      # Some macOS settings
+  	      window-theme = "dark";
+  	      shell-integration = "zsh";
+  	      gtk-titlebar=false;
+
+          background = config.lib.stylix.colors.base00; # for some reason does not work?
+  	      # # Disables ligatures
+  	      # font-feature = ["-liga" "-dlig" "-calt"];
+  	    };
+  };
+    # Zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    autocd = true;
+    syntaxHighlighting.enable = true;
+
+    initExtra = ''
+              export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/.nix-profile/lib/pkgconfig
+    '';
+
+    shellAliases = {
+      l = "ls -l";
+      ll = "ls -larh";
+      py = "python";
+      rr = "ranger";
+      mm = "micro";
+      rm = "rm -i";
+      summ = "sudo micro";
+      gg = "lazygit";
+      zz = "zeditor";
+      sfy = "spotify_player";
+      update = "sudo nixos-rebuild switch";
+    };
+  };
+
+  programs.zsh.oh-my-zsh = {
+  	enable = true;
+  	plugins = [ "git" "thefuck" ];
+  	# theme = "agnoster"; theme is configured via oh-my-posh
+  };
+
+  programs.oh-my-posh = {
+  	enable = true;
+  	enableZshIntegration = true;
+  	# useTheme = "aliens";
+  	settings = {
+  		version = 3;
+  		final_space = true;
+  		shell_integration = true;
+  		upgrade = false;
+
+		"$schema" =  "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json";
+
+  	    console_title_template =  "{{ .Folder }}";
+
+  		transient_prompt = {
+  		    background = "transparent";
+  		    foreground = "gray";
+  		    template = "{{ now | date \"15:04:05\" }} \\ue285 ";
+  		};
+
+  		blocks = [
+  			{
+  				type =  "prompt";
+  				alignment = "left";
+				trailing_diamond = "\\ue0b0";
+  				segments = [
+  					{
+  					 	foreground = "black";
+				     	background = "lightGreen";
+				     	leading_diamond =  "\\ue0b6";
+				     	trailing_diamond = "\\ue0b0";
+				     	background_templates = [
+				     		"{{ if eq .Type \"impure\" }}lightCyan{{ end }}"
+				     	];
+			          	style = "diamond";
+			          	type =  "nix-shell";
+			          	template = "\\uF313 {{ if eq .Type \"impure\" }}devshell{{else}}{{ .UserName }}{{ end }} ";
+			        }
+  					{
+  						type = "path";
+  						style = "diamond";
+  						# powerline_symbol = "\\ue0b0";
+  						leading_diamond = "\\ue0d7";
+				        foreground = "black";
+				        background = "white";
+				        properties = {
+				        	style = "mixed";
+				        };
+				        template =  "{{ if ne .Path \"~\" }} {{ .Path }} {{ end }}";
+				        # exclude_folders = [ "/home/matpac" ];
+					}
+					{
+  						type = "rust";
+  						style = "powerline";
+  						powerline_symbol = "\\ue0b0";
+				        foreground = "default";
+				        background = "red";
+				        display_mode = "context";
+				        template = " \\ue68b {{ .Full }} ";
+					}
+					{
+  						type = "git";
+  						style = "powerline";
+  						powerline_symbol = "\\ue0b0";
+				        foreground = "default";
+				        background = "blue";
+				        background_templates = [
+				            "{{ if or (.Working.Changed) (.Staging.Changed) }}yellow{{ end }}"
+				            "{{ if and (gt .Ahead 0) (gt .Behind 0) }}red{{ end }}"
+				        ];
+				        template = " {{ .UpstreamIcon }}{{ .HEAD }}{{if .BranchStatus }} {{ .BranchStatus }}{{ end }}{{ if .Working.Changed }}  {{ .Working.String }}{{ end }}{{ if and (.Working.Changed) (.Staging.Changed) }} |{{ end }}{{ if .Staging.Changed }}  {{ .Staging.String }}{{ end }}{{ if gt .StashCount 0 }}  {{ .StashCount }}{{ end }} ";
+				        properties = {
+				            fetch_status = true;
+				            fetch_upstream_icon = true;
+				            untracked_modes = {
+				              "/Users/user/Projects/oh-my-posh/" = "no";
+				            };
+				            source = "cli";
+				       };
+					}
+  				];
+  			}
+  			{
+  				type = "rprompt";
+  				alignment = "right";
+  				segments = [
+			        {
+			         background = "red";
+                     foreground = "default";
+                     trailing_diamond = "\\ue0b4";
+                     leading_diamond =  "\\ue0b6";
+                     properties = {
+                       always_enabled = false;
+                     };
+                     style = "diamond";
+                     template =  "{{ if gt .Code 0 }}\\uf00d {{ reason .Code }}{{ end }}";
+                     type = "status";
+                   }
+  				];
+  			}
+  		];
+
+  	};
+  };
+
+
+  services.spotifyd = {
+  	enable = true;
+  	# settings = {
+  	# 	global = {
+  	# 		username = "";
+  	#		password = "";
+  	# 	}
+  	# }
+  };
+
+
+	# /org/gnome/shell/extensions/openbar/winbcolor ['0.812', '0.631', '0.435']
+  dconf = {
+      enable = true;
+      settings = {
+          "org/gnome/shell" = {
+	           disable-user-extensions = false;
+	           disabled-extensions= [
+	           	   "Vitals@CoreCoding.com"
+	           	   "desktop-cube@schneegans.github.com"
+	           	   ###
+                   "openbar@neuromorph"
+                   "user-theme@gnome-shell-extensions.gcampax.github.com"
+                   "just-perfection-desktop@just-perfection" # crashes stylix on startup
+                   "dash-to-dock@micxgx.gmail.com"
+	           ];
+
+	           enabled-extensions = [
+	           	   "weatherornot@somepaulo.github.io"
+                   "trayIconsReloaded@selfmade.pl"
+                   "appindicatorsupport@rgcjonas.gmail.com"
+                   "blur-my-shell@aunetx"
+                   "accent-directories@taiwbi.com"
+                   "system-monitor@gnome-shell-extensions.gcampax.github.com"
+                   "dash-to-panel@jderose9.github.com"
+                   "mediacontrols@cliffniff.github.com"
+	           ];
+
+	           favorite-apps = [
+	                   # "firefox.desktop"
+	                   # "rust-rover.desktop"
+	                   # "webstorm.desktop"
+	                   # "code.desktop"
+	                   # "org.gnome.Terminal.desktop"
+	                   # "spotify.desktop"
+	                   # "discord.desktop"
+	                   # "steam.desktop"
+	           ];
+
+	      };
+
+	      "org/gnome/shell/extensions/openbar" = {
+	      		dark-bgcolor-wmax = ["0.0" "0.0" "1.0"];
+	      };
+	      	# lib.mkForce "${config.stylix.base16Scheme.base0E}";
+
+          "org/gnome/desktop/interface" = {
+          		# color-scheme = "prefer-dark"; # handeled by stylx
+          		cursor-size = 32;
+          		show-battery-percentage = true;
+          };
+
+          "org/gnome/desktop/wm/preferences" = {
+            workspace-names = [ "Main" ];
+          };
+
+          # handled by stylix
+		  # "org/gnome/desktop/background" = {
+		  #       picture-uri = "file:///home/matpac/.local/share/backgrounds/2025-01-23-21-05-42-wallpaper.jpg";
+		  #       picture-uri-dark = "file:///home/matpac/.local/share/backgrounds/2025-01-23-21-05-42-wallpaper.jpg";
+		  # };
+
+		  "org/gnome/desktop/screensaver" = {
+		        picture-uri = "file:///home/matpac/.local/share/backgrounds/2025-01-23-21-05-42-wallpaper.jpg";
+		        primary-color = "#3465a4";
+		        secondary-color = "#000000";
+		  };
+
+		  "org/gnome/settings-daemon/plugins/media-keys" = {
+		          # next = [ "<Shift><Control>n" ];
+		          # previous = [ "<Shift><Control>p" ];
+		          # play = [ "<Shift><Control>space" ];
+		          custom-keybindings = [
+		            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"		          ];
+		  };
+
+		  "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+		          name = "ghostty";
+		          command = "ghostty";
+		          binding = "<Super>t";
+		 };
+
+		 # "org/gnome/shell/extensions/user-theme" = {
+		 #   name = "gruvbox";
+		 # };
+
+      };
+    };
+
+  gtk = {
+    enable = true;
+
+    # theme = {
+    # 	name = "Tokyonight-Dark";
+    # 	package = pkgs.tokyonight-gtk-theme;
+    # };
+
+    # iconTheme = {
+    #	name = "Tokyonight-Dark";
+    #	package = pkgs.tokyo-night-gtk;
+    # };
+
+    # iconTheme = {
+    #   name = "GruvboxPlus";
+    #   package = gruvboxPlus;
+    # };
+
+    # theme = {
+    #   name = "gruvbox";
+    #   package = pkgs.gruvbox-gtk-theme;
+    # };
+
+    # theme = {
+    #	name = "adw-gtk3";
+    #	package = pkgs.adw-gtk3;
+    # };
+
+    # cursorTheme = {
+    #   name = "Polarnight-Cursors";
+    #   package = pkgs.polarnight-cursors;
+    # };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.activation.postConfigHook = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    /run/current-system/sw/bin/python3 /etc/nixos/json_fix.py
+  '';
+
+  home.activation.removeOhMyPoshConfig = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+    rm -f ~/.config/oh-my-posh/config.json
+  '';
+
+  stylix.targets.gtk.extraCss = ''
+        @define-color window_bg_color #${config.lib.stylix.colors.base00}b3;
+  '';
+
+
+   # xdg.configFile."oh-my-posh/config.json".source = lib.mkForce("/home/matpac/.config/oh-my-posh/config-fixed.json");
+   # xdg.configFile."oh-my-posh/config.json".force = true;
+
+  # home.sessionVariables.GTK_THEME = config.gtk.theme.name;
+}
