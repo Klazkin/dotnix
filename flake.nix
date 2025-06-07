9864cc6450f09863f4d68a66ea81853ef4aef9dd
matpac <matvei.matpac@gmail.com>
2025-06-07 13:43:20 +0300

    Revert "Auto-update ef71a98b-a789-456c-b817-dfd81896cb8e"
    This reverts commit f53b8d13223a7a927e5c59b9184f2233ae181e1f.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix/release-25.05";
+    # firefox-gnome-theme = { url = "github:rafaelmardojai/firefox-gnome-theme"; flake = false; };
+    stylix.url = "github:danth/stylix/release-24.11";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix/24.11";
   };
 
