af8a2f2880d118530a57f117cf6800b6d525aebc
matpac <matvei.matpac@gmail.com>
2025-06-08 09:02:40 +0300

    Auto-update e2fc5eee-5802-425d-a049-cff2fc3badc8

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,8 @@
     autoEnable = true;
 
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme =
+      "${pkgs.base16-schemes}/share/themes/gruvbox-dark-light.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
     image = ./wallpaper.jpg;
