8547bf824a8b4df495af8da1322c7a01f79d99ec
matpac <matvei.matpac@gmail.com>
2025-06-08 14:17:24 +0300

    Auto-update 89681feb-629a-4570-acea-9c522f3789de

--- home.nix
+++ home.nix
@@ -144,8 +144,8 @@ with lib.hm.gvariant;
       # This property will be used to generate settings.json:
       # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
       "editor.formatOnSave" = true;
-      "workbench.colorTheme" =
-        lib.mkForce "Gruvbox Material Dark"; # overwrites stylix theme
+      # "workbench.colorTheme" =
+      #   lib.mkForce "Gruvbox Material Dark"; # overwrites stylix theme
       # "workbench.colorTheme" = lib.mkForce "Ayu Mirage Bordered";
       "editor.cursorSmoothCaretAnimation" = "on";
       "workbench.iconTheme" = "ayu";
@@ -154,6 +154,7 @@ with lib.hm.gvariant;
       "godotTools.editorPath.godot4" =
         "/etc/profiles/per-user/matpac/bin/godot4";
       "editor.smoothScrolling" = true;
+      "glassit.alpha" = 176;
     };
   };
 
