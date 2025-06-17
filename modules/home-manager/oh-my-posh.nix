366151c3ed1b7e45efdefc00d135d1ed6c95ae66
matpac <matvei.matpac@gmail.com>
2025-06-17 17:04:44 +0300

    Auto-update 6ad50758-1373-4767-9646-19f48bee1fc8

--- modules/home-manager/oh-my-posh.nix
+++ modules/home-manager/oh-my-posh.nix
@@ -2,7 +2,7 @@
 
   # runs python script to fix \\ escapes to work as intended
   home.activation.postConfigHook = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    /run/current-system/sw/bin/python3 ./json_fix.py
+    /run/current-system/sw/bin/python3 ~/dotnix/json_fix.py
   '';
 
   # removes old config for oh-my-posh
