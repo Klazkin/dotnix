1295bfab8039d168664a76c0a75771209b1f45af
klazkin <matvei.matpac@gmail.com>
2026-06-02 18:45:51 +0300

    Auto-update 5d991bca-5c67-4d96-af30-06bfe23a0ea0

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
 
   inputs = {
     # NixOS official package source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix/release-25.11";
+    stylix.url = "github:danth/stylix/release-26.05";
 
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
